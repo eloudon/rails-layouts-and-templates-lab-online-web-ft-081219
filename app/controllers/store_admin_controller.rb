fe1c70e047136bb9a20c511b5e3f1b1d5703ab9d
Eric Loudon <loudon.eric@gmail.com>
2019-10-29 12:47:39 +0000

    Automatically backed up by Learn

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -1,14 +1,15 @@
 class StoreAdminController < ApplicationController
+layout "admin"
 
-  def home
-    render :layout => "admin"
-  end
+def home
+  render :home
+end
 
-  def orders
-    render :layout => "order_administration"
-  end
+def orders
+  render :layout => "order_administration"
+end
 
-  def invoice
-    render :layout => false
-  end
+def invoice
+  render :layout => false
+end
 end
